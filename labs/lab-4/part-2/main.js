7b90ac325a5dbd0506a26f2eb2e1c9c50917f37c
Phoenix00122 <jason.lauzon@dcmail.ca>
2026-07-19 19:30:52 -0400

    Updated Part 2:
    Added CSS + Setup Array

--- labs/lab-4/part-2/main.js
+++ labs/lab-4/part-2/main.js
@@ -3,3 +3,16 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+// Solution to Part 2
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+// Delcare the alt text for each image
+const altText = {
+    "pic1.jpg": "Closeup of a human eye",
+    "pic2.jpg": "Rock that looks like a wave",
+    "pic3.jpg": "Purple and white pansies",
+    "pic4.jpg": "Section of wall from an Ancient Egyptian tomb",
+    "pic5.jpg": "Large moth on a leaf"
+};
+
